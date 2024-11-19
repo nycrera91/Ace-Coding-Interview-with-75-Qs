458c6463b8783f2f9da5e34d0979e0ea0048bf05
alperenasa <nycrera@gmail.com>
2024-11-19 17:05:04 +0300

    605. Can Place Flowers

---
+++ src/CanPlaceFlowers605/Solution.java
@@ -0,0 +1,32 @@
+package CanPlaceFlowers605;
+
+public class Solution {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        if (n == 0) {
+            return true;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+
+                count++;
+
+                if (n == count) {
+                    return true;
+                }
+
+                i++; // Ekim tamamlandigi için sonraki bitkiyi gec
+            }
+
+        }
+
+        return false;
+
+    }
+
+}
